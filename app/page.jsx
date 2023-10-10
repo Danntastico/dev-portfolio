58611f48921afc1fa08c90381cd5ba2aad94392e
Dann <danilo.pena@ascential.com>
2023-10-10 16:40:26 -0500

    Companies section

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,11 @@
 import { Header, Timeline } from '@/components'
+import Companies from '@/components/Companies/Companies'
 
 export default function Home() {
   return (
     <main>
       <Header />
+      <Companies />
       <Timeline />
     </main>
   )
